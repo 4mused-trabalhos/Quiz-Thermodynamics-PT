b5338a443b9c5ccc972aa889d1e0ae0835f4d56d
Pedro Gomes <pedrogomes2000.3@gmail.com>
2020-01-07 17:45:16 +0000

    Created the global constants and variables

--- js/quiz.js
+++ js/quiz.js
@@ -1,3 +1,13 @@
+const HTML_Question = document.querySelector(".Q");
+const HTML_Score = document.querySelector(".S");
+const HTML_Button = document.querySelector(".Button");
+
+const HTML_A = [document.querySelector(".A1"), document.querySelector(".A2"), document.querySelector(".A3"), document.querySelector(".A4")];
+const HTML_R = [document.querySelector(".R1"), document.querySelector(".R2"), document.querySelector(".R3"), document.querySelector(".R4")];
+
+var questionNum = 0;
+var ScoreP = 0;
+
 class Question {
 
     
