673df044dc42bfcd63727ec9694de2c0afc347a4
Pedro Gomes <pedrogomes2000.3@gmail.com>
2020-01-08 22:20:02 +0000

    Quiz Logic Finished

--- js/quiz.js
+++ js/quiz.js
@@ -7,6 +7,7 @@ const HTML_R = [document.querySelector(".R1"), document.querySelector(".R2"), do
 
 var questionNum = 0;
 var ScoreP = 0;
+var radClass = "";
 
 class Question {
 
@@ -28,14 +29,17 @@ class Question {
 var Questions = [
 
     new Question("Em que ano e por quem foi construida a primeira máquina frigorifica?", "1856 por James Harrison", "1856 por Domelre", "1913 por James Harrison", "1913 por Domelre"),
-    new Question("Como funciona a máquina frigorifica?", "Passagem de calor de uma fonte fria para uma fonte quente", "Passagem de calor de uma fonte quente para uma fria", "Meio tendo entre as duas fontes", "Magic"),
+    new Question("Como funciona a máquina frigorifica?", "Passagem de calor de uma fonte fria para uma fonte quente", "Passagem de calor de uma fonte quente para uma fonte fria", "Meio termo entre as duas fontes", "Magic"),
     new Question("Quantas leis da termodinâmica existem?", "4", "5", "3", "2"),
     new Question("No que consiste a lei zero da termodinâmica?", "No tratamento das condições para a obtenção do equilibrio térmico", "Na transferência do calor do corpo mais quente para o mais frio, de forma espontânea", "Na tentativa de estabeleçer um ponto de referencia que determine a entropia", "No relacionamento do principio de conservação de energia"),
     new Question("No que consiste a primeira lei da termodinâmica?", "No relacionamento do principio de conservação de energia", "Na tentativa de estabeleçer um ponto de referencia que determine a entropia", "No tratamento das condições para a obtenção do equilibrio térmico", "Na transferência do calor do corpo mais quente para o mais frio, de forma espontânea"),
     new Question("No que consiste a segunda lei da termodinâmica?", "Na transferência do calor do corpo mais quente para o mais frio, de forma espontânea", "No tratamento das condições para a obtenção do equilibrio térmico", "Na tentativa de estabeleçer um ponto de referencia que determine a entropia", "No relacionamento do principio de conservação de energia"),
     new Question("No que consiste a terceira lei da termodinâmica?", "Na tentativa de estabeleçer um ponto de referencia que determine a entropia", "No relacionamento do principio de conservação de energia", "Na transferência do calor do corpo mais quente para o mais frio, de forma espontânea", "No tratamento das condições para a obtenção do equilibrio térmico"),
     new Question("Qual o objetivo do cooler do processador?", "Arrefecer o processador", "Nada", "Enfeite", "Ocupar espaço"),
-    new Question("Que tipo de refrigeração não existe", "Refrigeração elétrica", "Refrigeração a nitrogénio", "Rifregeração a água", "Rifrageração a ar")
+    new Question("Que tipo de refrigeração não existe?", "Refrigeração elétrica", "Refrigeração a nitrogénio", "Rifregeração a água", "Rifrageração a ar"),
+    new Question("O que é Energia Térmica?", "Energia diretamente associada à temperatura absoluta do corpo", "Energia total considerada no sistema", "Energia associada a um sistema termodinâmico onde ocorre interação entre corpos", "Energia diretamente associada à temperatura minima do corpo"),
+    new Question("O que é Energia Interna?", "Energia total considerada no sistema", "Energia diretamente associada à temperatura absoluta do corpo", "Energia associada a um sistema termodinâmico onde ocorre interação entre corpos", "Energia diretamente associada à temperatura minima do corpo"),
+    new Question("O que é Energia Potencial?", "Energia associada a um sistema termodinâmico onde ocorre interação entre corpos", "Energia diretamente associada à temperatura minima do corpo", "Energia diretamente associada à temperatura absoluta do corpo", "Energia total considerada no sistema")
 
 ];
 
@@ -84,52 +88,74 @@ function Display(QuestionsArray) {
     
 }
 
-function Score() {
+function ClassHandler(){
 
     for(l = 0; l < HTML_R.length; l++) {
+
 
         if(HTML_R[l].checked) {
 
             HTML_Button.disabled = true;
+            radClass = HTML_A[l].className;
 
             if(HTML_R[l].value == "R") {
 
-                var Class = HTML_A[l].className;
-                HTML_A[l].className += " correto"
-
+                HTML_A[l].className += " correto";
                 ScoreP += 100;
 
             }
             else {
-
-                var Class = HTML_R[l].className;
-                HTML_A[l].className += " errado"
-
+                HTML_A[l].className += " errado";
             }
 
-            setTimeout(() => { 
-                
-                HTML_A[l].className = Class;
-                HTML_Button.disabled = false;
-                
-                HTML_Score.textContent = "Pontuação: " + ScoreP;
-                Display(Questions);
-
-             }, 2500);
-
-            
-
+            setTimeout('setclass('  + l + ')', 2500);
+            setTimeout('uncheck('  + l + ')', 2500); 
+            setTimeout(function(){HTML_Score.textContent = "Pontuação: " + ScoreP;}, 2500);      
+    
         }
+    
+    }
+
+}
+
+function Score() {
+
+    if(questionNum < Questions.length) {
+
+        ClassHandler();
+        
+        setTimeout(function(){Display(Questions);}, 2500);
+        setTimeout(function(){HTML_Button.disabled = false;}, 2500);
+
+    }
+    else {
+
+        ClassHandler();
+
+        for(var i = 0, len = HTML_R.length; i < len; i++)
+        {   
+            HTML_R[i].disabled = false;
+        }
+
+        setTimeout(function(){alert("Pontuação: " + ScoreP);}, 2500);
 
     }
 
 }
 
-function main() {
+function setclass(num) {
+    HTML_A[num].className = radClass;
+}
 
+function uncheck(num) { 
+    HTML_R[num].checked = false;
+}
+
+function main() {
+    
     HTML_Score.textContent = "Pontuação: " + ScoreP;
 
     Questions = Random(Questions);
     Display(Questions);
-
+    
 }
